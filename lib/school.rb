6b5a8ad2f37bc5e103eca2538ba2b9df8eede510
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-21 00:02:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,11 @@
 class School 
   def initialize(school_name)
     @school_name = school_name
-    
-    def roster 
+  end
+  
+  def roster 
       @roster = {}
-    end
   end
-  
-  def roster=(roster)
-    @roster = roster 
-  end 
-  
-  def add_student(student_name, grade)
-    if roster.has_key?(grade) == false 
-      @roster[grade] = []
-    end 
-    @roster[grade] << student_name 
-  end
+
 end 
 
