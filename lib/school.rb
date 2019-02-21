d7409fcc72bb4c19b8a44fdb247d8dac3f00186e
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-21 00:13:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,12 @@
 class School 
-  attr_writer :roster
+  attr_accessor :roster
   
   def initialize(school_name)
     @school_name = school_name
   end
-  
-  def roster 
-    @roster = {}
-  end
 
   def add_student(student_name, grade)
-    roster[grade] = student_name 
+    @roster = {}
   end
 
 end 
